d8d3e16ec9156acffd6d766303b2c077b6b16166
Jack McCormack <jack.mccormack1234@gmail.com>
2024-08-19 18:03:40 -0400

    Added pixel art for character, animation, and changes movement back to tile based

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import { Component, HostListener, OnDestroy, OnInit } from '@angular/core';
 import { Tile } from '../tile';
 
 @Component({
@@ -6,18 +6,54 @@ import { Tile } from '../tile';
   templateUrl: './game.component.html',
   styleUrl: './game.component.css'
 })
-export class GameComponent implements OnInit {
+export class GameComponent implements OnInit, OnDestroy {
   rows = 10;
   cols = 10;
   tileSize = 50;
-
   tileMap: Tile[][] = [];
+
+  playerX = 0;
+  playerY = 0;
 
   playerRow = 0;
   playerCol = 0;
 
+  playerMoveSpeed = 1.75;
+
+  isPlayerMoving: boolean = false;
+  currentDirection: string = "";
+  playerImage = "assets/player_down.png";
+
+  playerDownFrame = -1;
+
+  private fps: number = 60;
+  private frameInterval: number = 1000 / this.fps; // Interval in milliseconds
+  private nextFrameTime: number = 0;
+  private isRunning: boolean = false;
+  private timerId: any;
+
+  private keyState: { [key: string]: boolean } = {};
+
   ngOnInit(): void {
     this.initializeTileMap();
+    this.startGameLoop();
+  }
+
+  ngOnDestroy(): void {
+    this.stopGameLoop();
+  }
+
+  startGameLoop() {
+    this.isRunning = true;
+    this.nextFrameTime = performance.now() + this.frameInterval;
+    this.gameLoop();
+  }
+
+  stopGameLoop() {
+    this.isRunning = false;
+    if (this.timerId) {
+      clearTimeout(this.timerId);
+    }
   }
 
   initializeTileMap() {
@@ -31,33 +67,157 @@ export class GameComponent implements OnInit {
     }
   }
 
-  movePlayer(direction: string) {
+  gameLoop() {
+    if (!this.isRunning) return;
+
+    const currentTime = performance.now();
+    const timeToNextFrame = this.nextFrameTime - currentTime;
+
+    this.timerId = setTimeout(() => {
+      this.update(); // Update game logic
+      this.nextFrameTime = currentTime + this.frameInterval;
+      this.gameLoop(); // Continue the loop
+    }, Math.max(timeToNextFrame, 0));
+  }
+
+  update() {
+    if (this.isPlayerMoving) {
+      this.playerDownFrame++;
+      this.updatePlayerImage();
+      this.updatePlayerPosition(this.currentDirection);
+      this.isPlayerMoving = !this.isPlayerAtPosition();
+    }
+    else {
+      this.playerDownFrame = -1;
+      this.updatePlayerImage();
+      this.currentDirection = this.getKeyState();
+
+      if (this.currentDirection !== "") {
+        this.playerDownFrame = 0;
+
+        this.isPlayerMoving = true;
+        this.updatePlayerImage();
+        this.updatePlayerTile(this.currentDirection);
+        this.updatePlayerPosition(this.currentDirection);
+      }
+    }
+  }
+
+  isPlayerAtPosition() {
+    return ((this.playerX === this.playerCol * this.tileSize) && (this.playerY === this.playerRow * this.tileSize));
+  }
+
+  updatePlayerTile(direction: string) {
     if (direction === 'up') this.playerRow--;
     if (direction === 'down') this.playerRow++;
     if (direction === 'left') this.playerCol--;
-    if (direction === 'right') this.playerCol++;
+    if (direction === 'right') this.playerCol++
 
     if (this.playerRow < 0) this.playerRow = 0;
-    if (this.playerRow >= this.rows) this.playerRow = this.rows - 1;
+    if (this.playerRow >= this.rows - 1) this.playerRow = (this.rows - 1);
     if (this.playerCol < 0) this.playerCol = 0;
-    if (this.playerCol >= this.cols) this.playerCol = this.cols - 1;
+    if (this.playerCol >= this.cols - 1) this.playerCol = (this.cols - 1);
+  }
+
+  updatePlayerPosition(direction: string) {
+    if (direction === "up") {
+      this.playerY -= this.playerMoveSpeed;
+      if (this.playerY < this.playerRow * this.tileSize) {
+        this.playerY = this.playerRow * this.tileSize;
+      }
+    }
+    else if (direction === "down") {
+      this.playerY += this.playerMoveSpeed;
+      if (this.playerY > this.playerRow * this.tileSize) {
+        this.playerY = this.playerRow * this.tileSize;
+      }
+    }
+    else if (direction === "left") {
+      this.playerX -= this.playerMoveSpeed;
+      if (this.playerX < this.playerCol * this.tileSize) {
+        this.playerX = this.playerCol * this.tileSize;
+      }
+    }
+    else if (direction === "right") {
+      this.playerX += this.playerMoveSpeed;
+      if (this.playerX > this.playerCol * this.tileSize) {
+        this.playerX = this.playerCol * this.tileSize;
+      }
+    }
+  }
+
+  updatePlayerImage() {
+    if (this.currentDirection === "up") {
+      this.playerImage = "assets/player_up.png";
+    }
+    else if (this.currentDirection === "down") {
+      console.log("update player image")
+      if (this.playerDownFrame === -1) {
+        this.playerImage = "assets/player_down.png";
+      }
+      else if (this.playerDownFrame <= 14) {
+        //console.log("Frame 1");
+        this.playerImage = "assets/player_down_walk_1.png";
+      }
+      else if (this.playerDownFrame > 14 && this.playerDownFrame <= 29) {
+        //console.log("Frame 2");
+        this.playerImage = "assets/player_down_walk_2.png";
+      }
+      else if (this.playerDownFrame > 19 && this.playerDownFrame <= 29) {
+        //console.log("Frame 3");
+        this.playerImage = "assets/player_down_run_1.png";
+      }
+      else if (this.playerDownFrame > 29 && this.playerDownFrame <= 39) {
+        this.playerImage = "assets/player_down_run_2.png";
+      }
+      else if (this.playerDownFrame > 39 && this.playerDownFrame <= 49) {
+        this.playerImage = "assets/player_down_run_1.png";
+      }
+      // this.playerImage = "assets/player_down.png";
+    }
+    else if (this.currentDirection === "left") {
+      this.playerImage = "assets/player_left.png";
+    }
+    else if (this.currentDirection === "right") {
+      this.playerImage = "assets/player_right.png";
+    }
+  }
+
+  getKeyState() {
+    let direction = "";
+    let total = 0;
+    if (this.keyState['ArrowUp']) {
+      direction = "up";
+      total++;
+    }
+    if (this.keyState['ArrowDown']) {
+      direction = "down";
+      total++;
+    }
+    if (this.keyState['ArrowLeft']) {
+      direction = "left";
+      total++;
+    }
+    if (this.keyState['ArrowRight']) {
+      direction = "right";
+      total++;
+    }
+
+    if (total > 1 || total === 0) {
+      return "";
+    }
+    else {
+      return direction;
+    }
   }
 
   @HostListener('window:keydown', ['$event'])
-  handleKeyboardEvent(event: KeyboardEvent) {
-    switch (event.key) {
-      case 'ArrowUp':
-        this.movePlayer('up');
-        break;
-      case 'ArrowDown':
-        this.movePlayer('down');
-        break;
-      case 'ArrowLeft':
-        this.movePlayer('left');
-        break;
-      case 'ArrowRight':
-        this.movePlayer('right');
-        break;
-    }
+  handleKeyDown(event: KeyboardEvent) {
+    this.keyState[event.key] = true;
+  }
+
+  @HostListener('window:keyup', ['$event'])
+  handleKeyUp(event: KeyboardEvent) {
+    this.keyState[event.key] = false;
   }
 }
