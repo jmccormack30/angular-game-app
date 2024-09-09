2a58992993ff833abc283fec4a6a0fce87bd6cf5
Jack McCormack <jmccormack@deposco.com>
2024-09-09 16:56:27 -0400

    wip collision detection

--- src/app/entities/tile.ts
+++ src/app/entities/tile.ts
@@ -1,9 +1,30 @@
 export class Tile {
+    private width = 50;
+    private height = 50;
+
     public tileName: string;
     public image: HTMLImageElement | null = null;
 
     constructor(tileName: string, image: HTMLImageElement | null) {
         this.tileName = tileName;
         this.image = image;
+    }
+
+    isPlayerCollision(tileX: number, tileY: number, playerX: number, playerY: number): boolean {
+        const playerStartY = playerY + 46;
+        const playerEndY = playerY + 96;
+
+        const playerStartX = playerX;
+        const playerEndX = playerX + 48;
+
+        return (this.isXCollision() && this.isYCollision());
+    }
+
+    isXCollision(): boolean {
+        return true;
+    }
+
+    isYCollision(): boolean {
+        return true;
     }
 }
